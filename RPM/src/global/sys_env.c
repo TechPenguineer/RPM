b41504ad585aab0a41b3c5f777a68cb58506ab64
Tech Penguin <85949246+TechPenguineer@users.noreply.github.com>
2021-08-05 10:53:01 -0400

    fixed to use 64/32 bit sys

--- RPM/src/global/sys_env.c
+++ RPM/src/global/sys_env.c
@@ -1,8 +1,6 @@
 //#include <stdlib.h>
-#include <iostream>
 #include "../handle.h"
 void createAppDataDir()
 {
    //char appdataDir[] = getenv("APPDATA");
-   print("hi");
 }
--- RPM/src/main.c
+++ RPM/src/main.c
@@ -1,8 +1,6 @@
-
 #include "handle.h"
 
 int main(int argc, char *argv[])
 {
-    // SYSTEM DEFAULTS
     createAppDataDir();
 }
