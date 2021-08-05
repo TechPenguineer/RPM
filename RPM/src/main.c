5adda380cb79060cfd02b815494b3bd5d7d9f7ee
Tech Penguin <85949246+TechPenguineer@users.noreply.github.com>
2021-08-05 11:54:35 -0400

    correctly read args

--- RPM/src/main.c
+++ RPM/src/main.c
@@ -1,12 +1,25 @@
 #include "handle.h"
 #include <stdio.h>
+#include <string.h>
 #include <ctype.h>
-int main(int argc, char *argv[])
+int main(int argc, char** argv)
 {
     // ADD DEPENDENCY
-    if (tolower(argv[1]) == "add" && tolower(argv[2])=="dep"&& tolower(argv[4])=="-v" && argc==3)
+    //if (strcmp(tolower(argv[1]),"add") == 1
+    //&& strcmp(tolower(argv[2]),"dep") == 1
+    //&& strcmp(tolower(argv[4]),"-v") == 1
+    //&& argc==3)
+    //{
+    //    printf("a");
+    //    //AppendDep(tolower(argv[3]), argv[5]);
+    //}
+    if (strcmp(argv[1], "add")==0 && strcmp(argv[2], "dep")==0 && argc==3 && strcmp(argv[4], "-v")==0 && argc==4 )
     {
-        AppendDep(tolower(argv[3]), argv[5]);
+        if(argc > 4)
+        {
+            printf("Too many arguments supplied");
+        }
+        printf("Adding ", argv[3], " Version ", argv[4]);
     }
     
 }
